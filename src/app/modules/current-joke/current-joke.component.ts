d30a9b866db0b40161f81684fce82bf936b5cb02
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-24 12:52:10 +0200

    add retrieving logic for getting jokes every 5 seconds

--- src/app/modules/current-joke/current-joke.component.ts
+++ src/app/modules/current-joke/current-joke.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { IJoke } from 'src/app/models/joke';
+import { DataServiceService } from 'src/app/shared/data-service.service';
 
 @Component({
   selector: 'pjk-current-joke',
   templateUrl: './current-joke.component.html',
-  styleUrls: ['./current-joke.component.scss']
+  styleUrls: ['./current-joke.component.scss'],
 })
 export class CurrentJokeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  currentJoke : IJoke = {
+    joke: '',
+    liked: true
   }
 
+  constructor(private dataService: DataServiceService) {}
+
+  ngOnInit(): void {
+    setInterval(
+      () =>
+        this.dataService.getJoke().subscribe((joke) => {
+          if (joke.type === 'single')
+            this.currentJoke!.joke = joke.joke as string;
+          else {
+            this.currentJoke!.joke = ((joke.setup as string) + ' ' +
+              joke.delivery) as string;
+          }
+          console.log(this.currentJoke?.joke);
+        }),
+      5000
+    );
+  }
 }
