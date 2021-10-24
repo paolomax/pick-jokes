34f96099130e72be2e4b2db2856225d20cffd4e2
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-24 19:09:54 +0200

    components' folders of joke's module are now subfolder of /modules/jokes

--- src/app/modules/jokes/jokes-routing.module.ts
+++ src/app/modules/jokes/jokes-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { JokesComponent } from './jokes.component';
+import { JokesComponent } from './jokes-component/jokes.component';
 
 const routes: Routes = [{ path: '', component: JokesComponent }];
 
--- src/app/modules/jokes/jokes.module.ts
+++ src/app/modules/jokes/jokes.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JokesRoutingModule } from './jokes-routing.module';
-import { JokesComponent } from './jokes.component';
-import { CurrentJokeComponent } from '../current-joke/current-joke.component';
-import { LikedJokesComponent } from '../liked-jokes/liked-jokes.component';
-import { DislikedJokesComponent } from '../disliked-jokes/disliked-jokes.component';
+import { JokesComponent } from './jokes-component/jokes.component';
+import { CurrentJokeComponent } from './current-joke/current-joke.component';
+import { LikedJokesComponent } from './liked-jokes/liked-jokes.component';
+import { DislikedJokesComponent } from './disliked-jokes/disliked-jokes.component';
 
 
 @NgModule({
